008a7bee89f4d3847423366a2f9054113e2d5d9d
Lubna <shaikhls2421@gmail.com>
2022-06-25 15:58:08 +0530

    runtime.txt

--- ecomsite/settings.py
+++ ecomsite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storage',
 
     'hotel',
 ]
@@ -136,7 +137,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT ='staticfiles'
+# STATIC_ROOT ='staticfiles'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
